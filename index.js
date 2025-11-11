d2b7586c281c7bf7cc8b17d52049044c7060729c
Prashanta Shrestha <Prashanta.Shrestha@cima.ca>
2025-11-11 14:41:20 -0500

    Changes

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const { Controller, Tag, TagGroup } = require("ethernet-ip");
-var Mqtt = require('azure-iot-device-mqtt').Mqtt;
-var DeviceClient = require('azure-iot-device').Client
-var Message = require('azure-iot-device').Message;
+// var Mqtt = require('azure-iot-device-mqtt').Mqtt;
+// var DeviceClient = require('azure-iot-device').Client
+// var Message = require('azure-iot-device').Message;
 
 var connectionString = process.env.CONN_STRING;
 console.log(connectionString);
-var client = DeviceClient.fromConnectionString(connectionString, Mqtt);
-var PLCIP = "10.166.2.105";
+// var client = DeviceClient.fromConnectionString(connectionString, Mqtt);
+var PLCIP = "10.70.110.110";
 const PLC = new Controller();
 const group = new TagGroup();
 console.log("Starting application ... ");
 
 // Add some tags to group
-group.add(new Tag("T_100")); // Controller Scope Tag
-group.add(new Tag("T_200")); // Controller Scope Tag
-group.add(new Tag("T_300")); // Controller Scope Tag
+// group.add(new Tag("T_200")); // Controller Scope Tag
+group.add(new Tag("SIM_FIT.CV")); // Controller Scope Tag
+group.add(new Tag("SIM_LIT.CV")); // Controller Scope Tag
+// group.add(new Tag("T_300")); // Controller Scope Tag
 
 console.log("Connecting to PLC @ " + PLCIP);
 
@@ -28,16 +29,17 @@ PLC.connect(PLCIP, 0).then(() => {
             group.forEach(tag => {
                 obj[ tag.name ] = tag.value;
             });
-            var message = new Message(JSON.stringify(obj));
-            console.log('Sending message: ' + message.getData());
+            console.log(obj);
+            // var message = new Message(JSON.stringify(obj));
+            // console.log('Sending message: ' + message.getData());
             // Send the message.
-            client.sendEvent(message, function (err) {
-              if (err) {
-                console.error('send error: ' + err.toString());
-              } else {
-                console.log('message sent');
-              }
-            });
+            // client.sendEvent(message, function (err) {
+            //   if (err) {
+            //     console.error('send error: ' + err.toString());
+            //   } else {
+            //     console.log('message sent');
+            //   }
+            // });
         });
     },1000);
 })
